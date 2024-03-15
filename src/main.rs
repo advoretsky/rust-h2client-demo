35e6fa1c74ada239681243fa73385fa1f72cdfc6
Alex Dvoretsky <advoretsky@gmail.com>
2024-03-15 15:53:22 +0200

    blocking fetch with h2 and h2 dangerous

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+mod tls;
+
+use std::error::Error;
+use std::sync::Arc;
 use bytes::Bytes;
 use async_channel::{Receiver, Sender};
-use std::time::Duration;
 use client::SendRequest;
-use h2::client;
+use h2::{client};
 use reqwest::Url;
 use tokio::net::{TcpStream};
-use tokio::time::sleep;
+use tokio_rustls::{rustls::ClientConfig, TlsConnector};
+use tokio_rustls::client::TlsStream;
+use tokio_rustls::rustls::pki_types::ServerName;
 
 #[tokio::main]
 async fn main() {
@@ -18,14 +23,14 @@ async fn main() {
     });
 
     // Number of handler tasks (change this value as needed)
-    let num_handlers = 3;
+    let num_handlers = 1;
 
     // Spawn multiple Tokio tasks for handling filenames concurrently
     let handler_tasks: Vec<_> = (0..num_handlers)
         .map(|i| {
             let receiver = rx.clone();
             tokio::spawn(async move {
-                handle_filenames(receiver, i, "https://44.223.28.16:443/").await;
+                handle_filenames(receiver, i, "https://filestorage.localdomain/").await;
             })
         })
         .collect();
@@ -38,72 +43,134 @@ async fn main() {
 
 async fn fetch_filenames(tx: Sender<String>) {
     // Simulating a continuous process of fetching filenames
-    for i in 0..10 {
-        let filename = format!("file_{}.txt", i);
+    for i in 1..10 {
+        let filename = format!("file{:02}.txt", i);
         tx.send(filename).await.expect("Failed to send filename");
-        sleep(Duration::from_secs(1)).await;
+        // sleep(Duration::from_secs(1)).await;
     }
 }
-async fn new_connection(tcp: TcpStream) -> Result<SendRequest<Bytes>, h2::Error> {
-    let (send_request, connection) = client::handshake(tcp).await?;
-    tokio::spawn(async move {
-        connection.await.unwrap();
-    });
-    Ok(send_request)
-}
-
 async fn handle_filenames(rx: Receiver<String>, id: usize, base_url: &str) {
-    let send_request: Option<SendRequest<Bytes>> = Default::default();
-    while let Ok(filename) = rx.recv().await {
-        loop { // TODO implement limited number of retries here
-            let send_request = send_request.clone();
-            let mut send_request = match send_request {
-                None => {
-                    connect_to_server(base_url).await
-                }
-                Some(v) => v
+    let base_url = match base_url.parse::<Url>() {
+        Ok(v) => v,
+        Err(err) => {
+            eprintln!("{}", err);
+            return
+        }
+    };
+    let h2 = connect_to_server(&base_url).await;
+    'filename: while let Ok(filename) = rx.recv().await {
+        
+        let uri = match base_url.join(&filename) {
+            Ok(v) => v,
+            Err(err) => {
+                eprintln!("error building file path for {}: {}", filename, err);
+                break 'filename;
             }
-                .ready().await.unwrap();
+        };
+        let request = match http::Request::builder()
+            .method("GET")
+            .uri(uri.as_str())
+            .body(()){
+            Ok(v) => v,
+            Err(err) => {
+                eprintln!("failed building a request: {}", err);
+                break
+            }
+        };
+        // loop of retries
+        loop { // TODO implement limited number of retries here
 
-            let mut uri = base_url.parse::<Url>().unwrap();
-            uri.set_path(&filename);
-            let request = http::Request::builder()
-                .uri(uri.as_str())
-                .body(())
-                .unwrap();
+            if let Err(err) = h2.clone().ready().await {
+                eprintln!("error waiting for SendRequest to become ready: {}", err);
+                continue 'filename
+            }
 
-            let (response, _) = send_request.send_request(request, true).unwrap();
-            let (head, mut body) = response.await.unwrap().into_parts();
-            println!("handler {:?} received response: {:?}", id, head); // TODO open a file
+            println!("sending request to fetch {}", uri.as_str());
+            let (response, _) = h2.clone().send_request(request.clone(), true).unwrap();
+            let (head, mut body) = match response.await {
+                Ok(r) => r.into_parts(),
+                Err(err) => {
+                    eprintln!("failed downloading file {}: {}", uri.as_str(), err);
+                    continue
+                }
+            };  // .unwrap().into_parts();
+            println!("handler {:?} received response: {:?}", id, head.status); // TODO open a file
             let mut flow_control = body.flow_control().clone();
             while let Some(chunk) = body.data().await {
                 let chunk = chunk.unwrap();
-                println!("RX: {:?}", chunk); // TODO write to the file
+                println!("RX: {:?}", chunk.len()); // TODO write to the file
 
                 // Let the server send more data.
                 let _ = flow_control.release_capacity(chunk.len());
             }
+            break
         }
     }
 }
 
-async fn connect_to_server(base_url: &str) -> SendRequest<Bytes> {
-    let url = Url::parse(base_url).expect("Failed to parse URL");
+async fn connect_to_server(url: &Url) -> SendRequest<Bytes> {
     let host = url.host_str().expect("URL does not contain a host");
     let port = url.port().unwrap_or(443);
 
     loop {
-        match TcpStream::connect(format!("{}:{}", host, port)).await {
-            Ok(tcp) => match new_connection(tcp).await {
-                Ok(connection) => return connection,
-                Err(err) => {
-                    eprintln!("Error establishing H2 connection: {}", err);
-                }
-            },
+        match attempt_connect(host, port).await {
+            Ok(v) => return v,
             Err(err) => {
-                eprintln!("Error establishing TCP connection: {}", err);
+                eprintln!("{}", err);
+                tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
             }
         }
-        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
     }
+}
+
+async fn attempt_connect(host: &str, port: u16) -> Result<SendRequest<Bytes>, Box<dyn Error + Send>> {
+    // // Load the system's root certificates
+    // let mut root_store = RootCertStore::empty();
+    // root_store.add_parsable_certificates(
+    //     &rustls_native_certs::load_native_certs().expect("failed to load native certs")
+    // );
+    //
+    // let mut config = ClientConfig::builder()
+    //     .with_safe_defaults()
+    //     // .with_custom_certificate_verifier(Arc::new(tls::NoCertificateVerification{}))
+    //     .with_root_certificates(root_store)
+    //     .with_no_client_auth();
+
+    let mut config = ClientConfig::builder()
+        .dangerous()
+        .with_custom_certificate_verifier(Arc::new(tls::SkipServerVerification{}))
+        .with_no_client_auth();
+
+    config.alpn_protocols.push(b"h2".to_vec()); // Enable HTTP/2 ALPN protocol negotiation.
+    let tls_connector = TlsConnector::from(Arc::new(config));
+
+    let authority = format!("{}:{}", host, port);
+    let tcp_stream = match TcpStream::connect(&authority).await {
+        Ok(v) => v,
+        Err(err) => return Err(Box::new(err))
+    };
+    let host_arc: String = host.to_owned();
+    let tls_stream = match tls_connector
+        .connect(
+            ServerName::try_from(host_arc)
+                .expect(&format!("invalid host name: {}", host)),
+            tcp_stream
+        ).await {
+        Ok(v) => v,
+        Err(err) => return Err(Box::new(err))
+    };
+    return match negotiate_h2_connection(tls_stream).await {
+        Ok(v) => Ok(v),
+        Err(err) => Err(Box::new(err))
+    };
+}
+
+async fn negotiate_h2_connection(tcp: TlsStream<TcpStream>) -> Result<SendRequest<Bytes>, h2::Error> {
+    let (send_request, connection) = client::handshake(tcp).await?;
+    tokio::spawn(async move {
+        if let Err(err) = connection.await {
+            eprintln!("Error occurred while waiting for connection to close: {}", err);
+        }
+    });
+    Ok(send_request)
 }
---
+++ src/tls.rs
@@ -0,0 +1,53 @@
+use std::fmt::{Debug, Formatter};
+use rustls::client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier};
+use rustls::{DigitallySignedStruct, Error, SignatureScheme};
+use rustls::pki_types::{CertificateDer, ServerName, UnixTime};
+
+// see https://quinn-rs.github.io/quinn/quinn/certificate.html
+pub(crate) struct SkipServerVerification;
+
+impl Debug for SkipServerVerification {
+    fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
+        return Ok(())
+        
+    }
+}
+
+impl ServerCertVerifier for SkipServerVerification {
+    fn verify_server_cert(
+        &self,
+        _: &CertificateDer<'_>,
+        _: &[CertificateDer<'_>],
+        _: &ServerName<'_>,
+        _: &[u8],
+        _: UnixTime,
+    ) -> Result<ServerCertVerified, Error> {
+        Ok(ServerCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(&self, _message: &[u8], _cert: &CertificateDer<'_>, _dss: &DigitallySignedStruct) -> Result<HandshakeSignatureValid, Error> {
+        return Ok(HandshakeSignatureValid::assertion());
+    }
+
+    fn verify_tls13_signature(&self, _message: &[u8], _cert: &CertificateDer<'_>, _dss: &DigitallySignedStruct) -> Result<HandshakeSignatureValid, Error> {
+        return Ok(HandshakeSignatureValid::assertion());
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
+        return vec![
+            SignatureScheme::RSA_PKCS1_SHA1,
+            SignatureScheme::ECDSA_SHA1_Legacy,
+            SignatureScheme::RSA_PKCS1_SHA256,
+            SignatureScheme::ECDSA_NISTP256_SHA256,
+            SignatureScheme::RSA_PKCS1_SHA384,
+            SignatureScheme::ECDSA_NISTP384_SHA384,
+            SignatureScheme::RSA_PKCS1_SHA512,
+            SignatureScheme::ECDSA_NISTP521_SHA512,
+            SignatureScheme::RSA_PSS_SHA256,
+            SignatureScheme::RSA_PSS_SHA384,
+            SignatureScheme::RSA_PSS_SHA512,
+            SignatureScheme::ED25519,
+            SignatureScheme::ED448,
+        ];
+    }
+}
